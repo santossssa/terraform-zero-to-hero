88e64686d84c5751a8c826636f3176be214c74bf
santossssa <142972362+santossssa@users.noreply.github.com>
2024-03-29 13:21:08 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-080e1f13689e07408"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0b8807f63387f54f3"
+    key_name = "terra"
 }
